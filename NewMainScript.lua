b16e73e1825efa80a9cca7d301787530a60ef76d
fuahsfpgagfsagf9we8gadsuea <112709811+fuahsfpgagfsagf9we8gadsuea@users.noreply.github.com>
2022-09-02 22:11:43 +0100

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -203,6 +203,11 @@ local Whitelist = GuiLibrary.CreateWindow({
 	["Icon"] = "vape/assets/RadarIcon1.png", 
 	["IconSize"] = 16
 })
+local Private = GuiLibrary.CreateWindow({
+	["Name"] = "Private", 
+	["Icon"] = "vape/assets/VapeLogo1.png", 
+	["IconSize"] = 16
+})
 local GuiLibrary = shared.GuiLibrary
 local LIB = function(tab, argstable) 
 	return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
